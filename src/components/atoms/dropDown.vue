7dc94b2872adbbdd457b1feccc0a0d8b7d04aee7
DanielRasho <ray22933@uvg.edu.gt>
2023-11-04 23:55:11 -0600

    Feat: Add dropDown component.

---
+++ src/components/atoms/dropDown.vue
@@ -0,0 +1,71 @@
+<template>
+  <div v-if="isActive" class="option">
+    <div
+      v-for="option in options"
+      @click="onSelect(option)"
+      :class="{ 'option-active': currentValue == option }"
+    >
+      {{ option }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+function onSelect(selectedOption) {
+  emit('optionSelected', selectedOption)
+}
+
+const emit = defineEmits(['optionSelected'])
+
+const props = defineProps({
+  currentValue: {
+    required: true,
+    type: String,
+    default: ''
+  },
+  isActive: {
+    required: true,
+    type: Boolean,
+    default: true
+  },
+
+  options: {
+    required: true,
+    type: Array,
+    default: ['option1', 'option2', 'option3']
+  },
+
+  width: {
+    required: false,
+    type: String,
+    default: '30ch'
+  }
+})
+</script>
+
+<style scoped>
+.option {
+  position: absolute;
+  top: calc(105% + 1rem);
+  left: 20%;
+  width: v-bind(width);
+  background: var(--primary);
+  box-shadow:
+    rgba(0, 0, 0, 0.8) 0px 19px 38px,
+    rgba(0, 0, 0, 0.8) 0px 15px 12px;
+  border-radius: 10px;
+  overflow: hidden;
+  z-index: 1;
+}
+.option div {
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.option-active,
+.option div:hover {
+  background-color: var(--text);
+  color: var(--primary);
+}
+</style>

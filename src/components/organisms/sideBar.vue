4ed230031c2481d84fe5bc4d8c341a32441ed8ef
DanielRasho <ray22933@uvg.edu.gt>
2023-11-05 11:18:19 -0600

    Feat: Create SideBar component.

---
+++ src/components/organisms/sideBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="sidebar" :class="{ active: isVisible }">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const isVisible = ref(true)
+</script>
+
+<style scoped>
+.sidebar {
+  height: 100vh;
+  width: 0;
+  padding: 0;
+
+  background-color: var(--background);
+  border-right: 1px solid black;
+
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+
+  transition: 0.3s ease-in-out;
+}
+
+.active {
+  width: 38ch;
+  padding: 3ch;
+}
+</style>

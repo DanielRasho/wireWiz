ca1f847120b070b9633dafa21133ba10e7f06265
DanielRasho <ray22933@uvg.edu.gt>
2023-11-04 23:53:38 -0600

    Fix : Add vue-click-outside-element to app component

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import vueClickOutsideElement from 'vue-click-outside-element'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vueClickOutsideElement)
 
 app.mount('#app')

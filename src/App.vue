b656e39ed9bf1598a7097b6a87404c5b60efb12b
DanielRasho <ray22933@uvg.edu.gt>
2023-11-04 23:51:48 -0600

    Feat: Add library for checking if user click outside a component.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
-
+import numberField from './components/atoms/numberField.vue'
+import selectField from './components/atoms/selectField.vue'
+import buttonPush from './components/atoms/buttonPush.vue'
+import numberFieldAdvanced from './components/molecules/numberFieldAdvance.vue'
 </script>
 
 <template>
   <main>
     <h1 class="font-title">Hola mundo</h1>
+    <select-field label="Element"></select-field>
+    <number-field label="Length" unit="m"></number-field>
+    <button-push> Hey there </button-push>
+    <number-field-advanced></number-field-advanced>
   </main>
 </template>
 
 <style scoped>
-
+main {
+  position: relative;
+}
 </style>

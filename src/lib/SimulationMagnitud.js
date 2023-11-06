392f4fe3c5ecfa62989f4a7bf23c831004e79732
DanielRasho <ray22933@uvg.edu.gt>
2023-11-05 23:53:27 -0600

    Fix: Renaming SimulationManitud.

---
+++ src/lib/SimulationMagnitud.js
@@ -0,0 +1,15 @@
+/**
+ * Constructs a magnitude to be used inside a simulation context.
+ *
+ * This class can be used to represent a value that the user needs to input.
+ * @param {Number} value The value of this magnitude
+ * @param {String} name The name of this magnitude
+ * @param {String} unit The unit of this magnitude
+ */
+export class SimulationMagnitude {
+  constructor(value, name, unit) {
+    this.value = value
+    this.name = name
+    this.unit = unit
+  }
+}
